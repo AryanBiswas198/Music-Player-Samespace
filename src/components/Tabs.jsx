148eb6f68eb0e2990cc5ac1c4f9650aaff0d1d0b
Aryan Biswas <aryanb198@gmail.com>
2024-06-16 15:48:51 +0530

    Tabs component responsive added

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 const Tabs = ({ activeTab, onTabChange }) => {
   return (
-    <div className="flex mb-6 text-white tracking-wide leading-8 font-bold gap-x-8">
+    <div className="hidden md:flex mb-6 text-white tracking-wide leading-8 font-bold gap-x-7 lg:gap-x-8">
       <button
-        className={`text-2xl ${activeTab === 'For You' ? 'text-white' : 'text-gray-500'}`}
+        className={`md:text-xl lg:text-2xl ${activeTab === 'For You' ? 'text-white' : 'text-gray-500'}`}
         onClick={() => onTabChange('For You')}
       >
         For You
       </button>
       <button
-        className={`text-2xl ${activeTab === 'Top Tracks' ? 'text-white' : 'text-gray-500'}`}
+        className={`md:text-xl lg:text-2xl ${activeTab === 'Top Tracks' ? 'text-white' : 'text-gray-500'}`}
         onClick={() => onTabChange('Top Tracks')}
       >
         Top Tracks
